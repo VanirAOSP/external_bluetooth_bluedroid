91cb4d89d80b1b01e9ef57d9741ec8c2774c7af0
nuclearmistake <nuclearmistake@gmail.com>
2015-03-11 22:11:46 -0400

    btlegatt test has missing initializers
    Change-Id: I586d25ea61db4fa6526bcf6ab163354fcc2fb6c4

--- test/blegatt_test/Android.mk
+++ test/blegatt_test/Android.mk
@@ -30,7 +30,7 @@ LOCAL_C_INCLUDES := . \
          $(bdroid_C_INCLUDES) \
          external/tinyxml2
 
-LOCAL_CFLAGS += -std=c99
+LOCAL_CFLAGS += -std=c99 -Wno-error=missing-field-initializers
 
 LOCAL_CFLAGS += $(bdroid_CFLAGS)
 LOCAL_MODULE_TAGS := debug optional
